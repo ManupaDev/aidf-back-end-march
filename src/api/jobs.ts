2731dde8dd126340b31f6720d83a1a39525e425d
ManupaDev <manupadevsuite@gmail.com>
2024-06-16 00:14:20 +0530

    feat: session 11 complete

--- src/api/jobs.ts
+++ src/api/jobs.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { createJob, deleteJob, getAllJobs, getJobById, updateJob } from "../application/jobs";
+import { ClerkExpressRequireAuth } from "@clerk/clerk-sdk-node";
 
 const jobsRouter = express.Router();
 
-jobsRouter.route("/").get(getAllJobs).post(createJob);
-jobsRouter.route("/:_id").get(getJobById).put(updateJob).delete(deleteJob);
+jobsRouter.route("/").get(getAllJobs).post(ClerkExpressRequireAuth({}), createJob);
+jobsRouter.route("/:_id").get(ClerkExpressRequireAuth({}), getJobById).put(updateJob).delete(deleteJob);
 
 export default jobsRouter;
