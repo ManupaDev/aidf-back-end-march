275c3cf3191796f1972d58f7d6243add5da0e474
ManupaDev <manupadevsuite@gmail.com>
2024-06-30 12:22:42 +0530

    feat: week 13 updated

--- src/application/rating.ts
+++ src/application/rating.ts
@@ -1,33 +1,37 @@
-import { log } from "console";
-import JobApplication from "../infrastructure/schemas/jobApplication";
+import { Types } from "mongoose";
 import OpenAI from "openai";
+import JobApplication from "../infrastructure/schemas/jobApplication";
 
-const client = new OpenAI({apiKey: process.env.OPENAI_API_KEY})
-export async function generateRating(jobApplicationId){
-    
-    const jobApplication = await JobApplication.findById(jobApplicationId).populate("job");
+const client = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
-    const content = `Role:${jobApplication?.job.title}, User Description: ${jobApplication?.answers.join(". ")}`
+export async function generateRating(jobApplicationId: Types.ObjectId) {
+  const jobApplication = await JobApplication.findById(
+    jobApplicationId
+  ).populate<{ job: { title: string; answers: string[] } }>("job");
 
-    const completion = await client.chat.completions.create(
-        {
-            messages:[{role:"user", content }],
-            model:"ft:gpt-3.5-turbo-0613:stemlink:fullstacktutorial:8dWQ9vUC"
-        }
-    )
+  const content = `Role:${
+    jobApplication?.job.title
+  }, User Description: ${jobApplication?.answers.join(". ")}`;
 
-    const strResponse = completion.choices[0].message.content;
-    console.log(strResponse);
-    if(!strResponse){
-        return;
-    }
+  const completion = await client.chat.completions.create({
+    messages: [{ role: "user", content }],
+    model: "ft:gpt-3.5-turbo-0613:stemlink:fullstacktutorial:8dWQ9vUC",
+  });
 
-    const response = JSON.parse(strResponse);
-    
-    if(!response.rate){
-        return
-    }
+  const strResponse = completion.choices[0].message.content;
+  console.log(strResponse);
+  if (!strResponse) {
+    return;
+  }
 
-    await JobApplication.findOneAndUpdate({_id:jobApplicationId},{rating:response.rate})
+  const response = JSON.parse(strResponse);
 
+  if (!response.rate) {
+    return;
+  }
+
+  await JobApplication.findOneAndUpdate(
+    { _id: jobApplicationId },
+    { rating: response.rate }
+  );
 }
